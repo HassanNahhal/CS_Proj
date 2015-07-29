1d05cca6904027c687c8cc62e0a98ec36e718c56
Changho Choi <cch.choi@gmail.com>
2015-07-29 00:53:35 -0400

    Add GameBoard, Cell Class

--- SourceCode/ConsoleApplication1/ConsoleApplication1/Cell.cs
+++ SourceCode/ConsoleApplication1/ConsoleApplication1/Cell.cs
@@ -1,11 +1,9 @@
-﻿/**
- * 
- * 
- * Done by Changho Choi
- * 
- * 
- * 
- */
+﻿/* Cell.cs
+ * Final Project
+ * Revision History 
+ * Changho Choi, 2015.07.28: Created 
+*/
+
 
 using System;
 using System.Collections.Generic;
@@ -15,7 +13,66 @@ using System.Threading.Tasks;
 
 namespace Monopoly
 {
-    class Cell
+    public class Cell
     {
+        Boolean available;
+        String cellName;
+        int index;
+        int cellPrice;
+        Player owner;
+
+        public Cell(int index, String cellName, int cellPrice)
+        {
+            this.index = index;
+            available = true;
+            owner = null;
+            this.cellName = cellName;
+            this.cellPrice = cellPrice;
+        }
+
+        public Cell()
+        {
+        }
+
+        public int GetPrice()
+        {
+            return this.cellPrice;
+        }
+        
+        public Boolean IsAvailable()
+        {
+            return available;
+        }
+
+        public void SetAvailable(Boolean available)
+        {
+            this.available = available;
+        }
+        
+        public String GetName()
+        {
+           return  this.cellName;
+        }
+
+
+        public void SetName(String cellName)
+        {
+             this.cellName = cellName;
+        }
+
+        public Player GetOwner()
+        {
+            return this.owner;
+        }
+
+        public void SetOwner(Player owner)
+        {
+            this.owner = owner;
+        }
+
+        public int GetIndex()
+        {
+            return this.index;
+        }
     }
 }
--- SourceCode/ConsoleApplication1/ConsoleApplication1/GameBoard.cs
+++ SourceCode/ConsoleApplication1/ConsoleApplication1/GameBoard.cs
@@ -1,11 +1,9 @@
-﻿/**
- * 
- * 
- * Done by Changho Choi
- * 
- * 
- * 
- */
+﻿/* GameBoard.cs
+ * Final Project
+ * Revision History 
+ * Changho Choi, 2015.07.28: Created 
+*/
+
 
 using System;
 using System.Collections.Generic;
@@ -15,7 +13,49 @@ using System.Threading.Tasks;
 
 namespace Monopoly
 {
-    class GameBoard
+    public class GameBoard
     {
+        const int NUM_OF_CELL = 40;
+        const int FIX_CELL_PRICE = 2000;
+
+        List<Cell> cells = new List<Cell>();
+
+        public GameBoard()
+        {
+            for (int index = 1; index <= NUM_OF_CELL; index++)
+            {
+                cells.Add(new Cell(index, "Cell" + index, FIX_CELL_PRICE));
+            }
+        }
+
+        public Cell AddCell(Cell curPosition, int distance)
+        {
+            return cells.ElementAt(((curPosition.GetIndex() + distance) % NUM_OF_CELL)-1);
+        }
+
+        public Cell GetCell(int index)
+        {
+            return cells.ElementAt(index-1);
+        }
+
+        public int GetCellNumber(Cell curPosition)
+        {
+            return curPosition.GetIndex();
+        }
+
+        public void GetPropertiesInMonopoly()
+        {
+            //Not Need, Will be erased
+        }
+
+        public void QueryCell()
+        {
+            //Not Need, Will be erased
+        }
+
+        public void QueryCellIndex()
+        {
+            //Not Need, Will be erased
+        }
     }
 }
