e4798d3bc95a36e25227d96a893e90ac95a8a71d
Skim0082 <Skim0082@conestogac.on.ca>
2015-08-04 09:45:54 -0400

    Update Questions.cs

--- SourceCode/ConsoleApplication1/ConsoleApplication1/Questions.cs
+++ SourceCode/ConsoleApplication1/ConsoleApplication1/Questions.cs
@@ -23,6 +23,8 @@ namespace Monopoly
         // who intiate the possition of the player and what is the possition 0 or 1 ??
 
         // who has the fuction that moves a player from one cell to another ?
+        
+        // who has the function that set the player's postion after RollDie?
 
 
     }
