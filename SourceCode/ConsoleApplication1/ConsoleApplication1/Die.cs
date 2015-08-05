70d1356d7a8913ed2fe48a2b532fd7734789bd76
Hassan Nahhal <nahhal.hasan@gmail.com>
2015-08-04 22:31:07 -0400

    added minValue
    minValue=1 , so random number will not be 0

--- SourceCode/ConsoleApplication1/ConsoleApplication1/Die.cs
+++ SourceCode/ConsoleApplication1/ConsoleApplication1/Die.cs
@@ -15,6 +15,8 @@ namespace Monopoly
     public class Die
     {
         private const int maxNumber = 7;
+        private const int minNumber = 1;
+
         private int dieFirstFaceValue;
         private int dieSecondFaceValue;
 
@@ -31,12 +33,12 @@ namespace Monopoly
         {
 
             Console.WriteLine ( "The first Die is being rolled" );
-            dieFirstFaceValue = random.Next(maxNumber);
+            dieFirstFaceValue = random.Next(minNumber,maxNumber);
             Console.WriteLine ( "The first Die = " + dieFirstFaceValue );
             Console.WriteLine ();
 
             Console.WriteLine ( "The second Die is being rolled" );
-            dieSecondFaceValue= random.Next(maxNumber);
+            dieSecondFaceValue = random.Next ( minNumber , maxNumber );
             Console.WriteLine ( "The second Die = " + dieSecondFaceValue );
 
             return dieFirstFaceValue + dieSecondFaceValue;
